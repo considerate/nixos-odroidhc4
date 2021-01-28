d7f6115b7bfffaf3c68f31140989a5d2d1c15785
Viktor Kronvall <viktor.kronvall@gmail.com>
2021-01-29 03:15:59 +0900

    Use fetchTarball when fetching nixpkgs

--- modules/nixpkgs/default.nix
+++ modules/nixpkgs/default.nix
@@ -1,5 +1,4 @@
-builtins.fetchGit {
-  url = "git+ssh://github.com/NixOS/nixpkgs.git";
-  ref = "release-20.09";
-  rev = "352481146d87aa2d277ea696657aa7db5378b90f";
+builtins.fetchTarball {
+  url = "https://github.com/NixOS/nixpkgs/archive/352481146d87aa2d277ea696657aa7db5378b90f.tar.gz";
+  sha256 = "0fyi26dcpprbh0zccwjjp6spikbbj8dsal2aa8h1qnw41a4fjjxm";
 }
