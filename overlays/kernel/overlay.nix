a516fba372fa6bd89fb62c6359117dc78a84eb9f
Viktor Kronvall <viktor.kronvall@gmail.com>
2021-08-01 06:19:50 +0900

    Build against mainline kernel

--- overlays/kernel/overlay.nix
+++ overlays/kernel/overlay.nix
@@ -1,13 +1,46 @@
 final: prev: {
-  linux_hardkernel = final.callPackage ./kernel.nix {
-    kernelPatches = [
-      # The kernel fails to cross-compile due to:
-      # 1. warnings caused by different interpretation of function pointers
-      # 2. NR_CPUS causing stack overflows when allocating cpu_topology
-      # 3. The IRBLASTER module not registering syscalls correctly
+  linux_hardkernel = final.linux.override {
+    # Configuration provided by elatllat
+    # https://forum.odroid.com/viewtopic.php?f=176&t=33993&p=261833#p261833
+    extraConfig = ''
+      MDIO_BUS_MUX_MESON_G12A y
+      ARCH_MESON y
 
-      # The following patch makes the above warnings non-errors, decreases NR_CPUS to 4 and disables the IRBLASTER module.
-      ({ name = "hardkernel-patches"; patch = ./kernel.diff; })
-    ];
+      BPF_SYSCALL y
+      CGROUP_BPF y
+      BPF_EVENTS y
+      SQUASHFS_XZ y
+      BLK_DEV_DM y
+      FUSE_FS y
+      SQUASHFS_XZ y
+      PNFS_BLOCK y
+      CRYPTO_CBC y
+      CRYPTO_ECB y
+      CRYPTO_XTS y
+      CRYPTO_USER_API y
+      CRYPTO_USER_API_HASH y
+      CRYPTO_USER_API_SKCIPHER y
+      USB_UAS y
+      PWM_MESON y
+
+      BLK_DEV_MD m
+      MD_AUTODETECT m
+      MD_LINEAR m
+      MD_RAID0 m
+      MD_RAID1 m
+      MD_RAID10 m
+      MD_RAID456 m
+      XOR_BLOCKS m
+      ASYNC_CORE m
+      ASYNC_MEMCPY m
+      ASYNC_XOR m
+      ASYNC_PQ m
+      ASYNC_RAID6_RECOV m
+      RAID6_PQ m
+      LIBCRC32C m
+
+      WIRELESS n
+      WLAN n
+    '';
   };
 }
